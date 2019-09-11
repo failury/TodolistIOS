c6349d2b761e0914cb36de81c2887cde38bd3dbc
failury <failury29@gmail.com>
2019-09-11 19:07:26 +0800

    added cells and checkmark

---
+++ TodolistIOS/Controller/TDLViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TodolistIOS
+//
+//  Created by failury on 9/10/19.
+//  Copyright © 2019 Ruifeng Zhang. All rights reserved.
+//
+
+import UIKit
+
+class TDLViewController: UITableViewController {
+    let listArray = ["first row", "second row", "third row"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        cell.textLabel?.text = listArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+    }
+    
+}
+
