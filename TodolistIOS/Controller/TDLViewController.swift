ceaca7029d24cf5266b6a161ab8a37db337dc403
failury <failury29@gmail.com>
2019-09-13 00:50:53 +0800

    implemented the data model, the adding function is broken

--- TodolistIOS/Controller/TDLViewController.swift
+++ TodolistIOS/Controller/TDLViewController.swift
@@ -9,40 +9,39 @@
 import UIKit
 
 class TDLViewController: UITableViewController {
-    var listArray = ["first row", "second row", "third row"]
+    var listArray = [TodoItem]()
+    //MARK: load local data
     let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let temp = defaults.array(forKey: "TodoListArray") as? [String]{
-            listArray = temp
+        if let items = defaults.array(forKey: "TodoListArray") as? [TodoItem] {
+            listArray = items
         }
     }
-    
+    //MARK: Override TableView methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-        cell.textLabel?.text = listArray[indexPath.row]
+        cell.textLabel?.text = listArray[indexPath.row].title
+        cell.accessoryType = listArray[indexPath.row].mark ? .checkmark : .none
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        listArray[indexPath.row].mark = !listArray[indexPath.row].mark
+        tableView.reloadData()
     }
-    
+    //MARK: Plus button
     @IBAction func plusButtonPreseed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let addingPrompt = UIAlertController(title: "Add new Todo", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            print(textField.text!)
-            self.listArray.append(textField.text!)
-            self.defaults.setValue(self.listArray, forKey: "TodoListArray")
+            let item = TodoItem()
+            item.title = String(textField.text!)
+            self.listArray.append(item)
+            self.defaults.set(self.listArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         addingPrompt.addTextField { (textfield) in
---
+++ TodolistIOS/Model/TodoItem.swift
@@ -0,0 +1,13 @@
+//
+//  TodoItem.swift
+//  TodolistIOS
+//
+//  Created by failury on 9/12/19.
+//  Copyright © 2019 Ruifeng Zhang. All rights reserved.
+//
+
+import Foundation
+class TodoItem{
+    var title: String = ""
+    var mark : Bool = false
+}
