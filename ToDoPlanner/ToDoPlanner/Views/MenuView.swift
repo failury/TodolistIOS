89ace374c46c9bc23f203545fb418ffcdb844e8b
Wilson0925 <54805745+Wilson0925@users.noreply.github.com>
2020-02-28 19:16:41 -0800

    Create a Menuview

---
+++ ToDoPlanner/ToDoPlanner/Views/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  ToDoPlanner
+//
+//  Created by 杨梓蔚 on 2/28/20.
+//  Copyright © 2020 iDEX. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
